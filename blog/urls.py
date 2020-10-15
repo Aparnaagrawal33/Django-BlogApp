d8cc8ca4449ae6ff131cc26786d60b238c7042f0
aparnaagrawal33 <aparnaagrawal33@gmail.com>
2020-10-15 20:58:34 +0530

    Models Done

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
 	path("",views.home, name="blog-home"),
-	path=("/blog", views.blog, name = "blog-about"),
-		]
+	path("about/", views.about, name = "blog-about"),
+]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,38 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
-def home(self):
-	return HttpResponse('<h1>Blog home</h1>')
+'''
+def home(request):
+	return render(request,'blog/home.html', {'title':'Home'})
 
-def about(self):
-	return HttpResponse('<h1>Blog about page</h1>')
+def about(request):
+	return render(request,'blog/about.html', {'title':'about'})
+	'''
 
+'''posts = [
+    {
+        'author': 'Aparna Agrawal',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'August 27, 2020'
+    },
+    {
+        'author': 'Shriti sharan',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'September 1, 2020'
+    }
+]'''
+
+
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
